e9d091f121ed5ed074e2236aecea22baa98eac48
victo <victo@DESKTOP-UNH4CFU>
2022-11-15 00:16:19 -0600

    Inicio de Manejo de Errores

---
+++ Tema02/src/com/openbootcamp/ManejoDeErrores.java
@@ -0,0 +1,24 @@
+package com.openbootcamp;
+
+public class ManejoDeErrores {
+
+	public static void main(String[] args) {
+		String estacion= "primavera";
+	
+	if (estacion=="primavera") {
+				System.out.println("Es primavera ");
+	}
+	
+	else if (estacion=="verano") {
+			System.out.println("Es verano ");
+	}
+	else {
+		System.out.println("Es otra estación.....");
+	}
+
+	}
+
+}
+
+
+
