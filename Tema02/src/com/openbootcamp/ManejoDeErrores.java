42fa67639525a9791d7c2951fb48a45e1e1b31da
VictorGamezP <victor.gamezp@hotmail.com>
2022-11-17 17:59:20 -0600

    Manejo de errores

--- Tema02/src/com/openbootcamp/ManejoDeErrores.java
+++ Tema02/src/com/openbootcamp/ManejoDeErrores.java
@@ -2,21 +2,7 @@ package com.openbootcamp;
 
 public class ManejoDeErrores {
 
-	public static void main(String[] args) {
-		String estacion= "primavera";
 	
-	if (estacion=="primavera") {
-				System.out.println("Es primavera ");
-	}
-	
-	else if (estacion=="verano") {
-			System.out.println("Es verano ");
-	}
-	else {
-		System.out.println("Es otra estación.....");
-	}
-
-	}
 
 }
 
